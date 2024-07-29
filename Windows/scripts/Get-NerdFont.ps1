50b097f7c7ac8cf4209c9d9c82a4a2a6b81e42b0
Mark Arnott <209603+markarnott@users.noreply.github.com>
2024-07-29 16:44:13 -0400

    update nerd font script

--- Windows/scripts/Get-NerdFont.ps1
+++ Windows/scripts/Get-NerdFont.ps1
@@ -15,12 +15,20 @@ Push-Location NerdFont
 
 curl -o FiraCode.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/$MyFontRelease"
 
+& "C:\Program Files\7-Zip\7z.exe" x .\FiraCode.zip
+<# 
+# The nerdfont install script
+# Didn't work on first try.  Is the juice worth the squeeze? 
+# you can install from the file explorer right-click menu
+
 New-Item patched-fonts -ItemType Directory
 Set-Location patched-fonts
 
 & "C:\Program Files\7-Zip\7z.exe" x ..\FiraCode.zip 
 Set-Location ..
 
-#curl -o install.ps1 https://github.com/ryanoasis/nerd-fonts/blob/master/install.ps1
+
+curl -o install.ps1 https://github.com/ryanoasis/nerd-fonts/blob/master/install.ps1
+#>
 Pop-Location
 
